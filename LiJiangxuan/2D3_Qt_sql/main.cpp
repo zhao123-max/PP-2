c9138198d4c7fea6ebc915275b86143a37d41a78
JiangxuanLi <jonathan1214@foxmail.com>
2020-07-22 19:53:33 +0800

    增加访问设备示例

--- LiJiangxuan/2D3_Qt_sql/main.cpp
+++ LiJiangxuan/2D3_Qt_sql/main.cpp
@@ -1,9 +1,29 @@
 #include <QApplication>
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
+#include <QSqlQueryModel>
+#include <QTableView>
 #include <QDebug>
 #include <QSqlError>
 #include <QDateTime>
+
+class PatientModel: public QSqlQueryModel
+{
+public:
+    PatientModel()
+    {
+        this->setQuery("SELECT *FROM patient");
+    }
+
+    Qt::ItemFlags flags(const QModelIndex &index) const override
+    {
+//        qDebug()<<"i am here"<<index.row()<<index.column();
+        Qt::ItemFlags flags = QSqlQueryModel::flags(index);
+        if (index.column() == 1 || index.column() == 2)
+            flags |= Qt::ItemIsEditable;
+        return flags;
+    }
+};
 
 
 int main(int argc, char *argv[])
@@ -53,44 +73,86 @@ int main(int argc, char *argv[])
             qDebug()<<name<<mobile;
         }
 
-        // 模拟终端
-//        query.exec("INSERT INTO sample (value, time) VALUES (x'2000', '2020-07-22 10:30:02')");
-
-        // 批量添加数据
-        query.prepare("INSERT INTO sample (value, time) VALUES (:array, :time)");
-
-        QByteArray waves;
-        waves.resize(10*2);
-        for (int i = 0; i < 10; i++)
-        {
-            waves[i*2] = 2020 & 0xFF; // 低八位
-            waves[i*2+1] = 2020 >> 8; // 高8位
-        }
-        query.bindValue(":array", waves);
-        query.bindValue(":time", QDateTime::currentDateTime());
-        bool queryOk = query.exec();
-        if (!queryOk) qDebug()<<query.lastError();
-        qDebug()<<query.lastQuery();
-
         //  工作站从数据库读取数据
         query.prepare("SELECT * FROM sample WHERE time > :start and time < :end");
         query.bindValue(":start", "2020-07-22 11:00:00");
         query.bindValue(":end", "2020-07-22 12:00:00");
 
+        bool queryOk;
         queryOk = query.exec();
         if(queryOk)
         {
-            qDebug()<<query.size();
+//            qDebug()<<query.size();
             while(query.next())
             {
-                QByteArray waveData = query.value("value").toByteArray();
+//                QByteArray waveData = query.value("value").toByteArray();
                 // todo, 画波形
-                qDebug()<<query.size()<<waveData;
+//                qDebug()<<query.size()<<waveData;
+                qDebug()<<"得到波形数据";
             }
         }else
         {
-            qDebug()<<query.lastError();
+            qDebug()<<"读取错误"<<query.lastError();
         }
+ /******************** 下午 *************/
+    // 获取当前设备是否已在设备表中
+        query.prepare("SELECT * from device "
+                      "WHERE serial = :serial");
+        query.bindValue(":serial", "DEV-007");
+
+        int dev_id = 1;
+        if(query.exec())
+        {
+            qDebug()<<"size"<<query.size();
+            if(query.size() > 0)
+            {
+                query.next();
+                qDebug()<<"设备已存在";
+                dev_id = query.value("dev_id").toInt();
+                qDebug()<<"当前设备编号："<<dev_id;
+            }
+            else
+            {
+                query.prepare("INSERT INTO device (serial)"
+                              "VALUES (:serial)");
+                query.bindValue(":serial", "DEV-007");
+                if (!query.exec())
+                {
+                    qDebug("设备注册失败！");
+                }
+                else
+                {
+                    qDebug("设备注册成功！");
+                }
+            }
+        }else
+        {
+            qDebug()<<"查看设备错误";
+        }
+
+        // 模拟终端设备，上传数据波形
+        // 方法2 bindValue
+        query.prepare("INSERT INTO sample (value, time, dev_id) VALUES (:array, :time, :dev_id)");
+        // 与数据库中数据类型一致的十六进制数组
+        short samples[3] = {2000,2001,2002};
+        QByteArray waves2((char*)samples, sizeof(samples));
+
+        query.bindValue(":array", waves2);
+        // 绑定当前时间
+        query.bindValue(":time", QDateTime::currentDateTime());
+        // 绑定设备ID
+        query.bindValue(":dev_id", dev_id);
+        // 执行sql语句
+        queryOk = query.exec();
+        if (queryOk) qDebug()<<"写波形成功";
+        else qDebug()<<"写波形错误："<<query.lastError();
+
+        // 每15s更新一次refresh 字段 判定离线条件为refresh时间与当前时间差值超过20s
+        query.prepare("UPDATE `medical_monitor1`.`device` SET refresh = NOW() WHERE dev_id = :dev_id");
+        query.bindValue(":dev_id", 1);
+        queryOk = query.exec();
+        if(!queryOk)
+            qDebug()<<"更新设备在线状态错误";
     }
     return a.exec();
 }
